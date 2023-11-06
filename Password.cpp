88c196c60c966679f9d101767082a07654ea5a9f
Cy <cbaca1@csuchico.edu>
2023-11-06 11:51:18 -0800

    Added has_mixed_case to Password class and test cases to PasswordTest.cpp

--- Password.cpp
+++ Password.cpp
@@ -10,12 +10,29 @@ using std::string;
   'z' and any ASCII characters are allowed.
 */
 int Password::count_leading_characters(string phrase){
-  int repetition = 1;
-  int index = 0;
+    int repetition = 1;
+    int index = 0;
 
-  while( index < phrase.length()-1 && phrase[index] == phrase[index+1] ){
-    repetition++;
-    index++;
-  }
-  return repetition;
+    while( index < phrase.length()-1 && phrase[index] == phrase[index+1] ){
+        repetition++;
+        index++;
+    }
+    return repetition;
+}
+
+bool Password::has_mixed_case(string phrase)
+{
+    if (phrase.empty())
+        return false;
+
+    bool has_lower = false;
+    bool has_upper = false;
+    for (int i = 0; i < phrase.length(); ++i) {
+        if (phrase[i] >= 0x61 && phrase[i] <= 0x7a)
+            has_lower = true;
+        if (phrase[i] >= 0x41 && phrase[i] <= 0x5a)
+            has_upper = true;
+    }
+
+    return has_lower && has_upper;
 }
--- PasswordTest.cpp
+++ PasswordTest.cpp
@@ -5,22 +5,110 @@
 #include <gtest/gtest.h>
 #include "Password.h"
 
-class PracticeTest : public ::testing::Test
+class PasswordTest : public ::testing::Test
 {
-	protected:
-		PracticeTest(){} //constructor runs before each test
-		virtual ~PracticeTest(){} //destructor cleans up after tests
-		virtual void SetUp(){} //sets up before each test (after constructor)
-		virtual void TearDown(){} //clean up after each test, (before destructor)
+    protected:
+    PasswordTest(){} //constructor runs before each test
+    virtual ~PasswordTest(){} //destructor cleans up after tests
+    virtual void SetUp(){} //sets up before each test (after constructor)
+    virtual void TearDown(){} //clean up after each test, (before destructor)
 };
 
 TEST(PasswordTest, smoke_test)
 {
     ASSERT_TRUE( 1 == 1 );
 }
+
+TEST(PasswordTest, four_leading_char_password)
+{
+    Password my_password;
+    int actual = my_password.count_leading_characters("ZZZZjklasdj");
+    ASSERT_EQ(4,actual);
+}
+
+TEST(PasswordTest, one_leading_char_password)
+{
+    Password my_password;
+    int actual = my_password.count_leading_characters("Zjklasdj");
+    ASSERT_EQ(1,actual);
+}
+
 TEST(PasswordTest, single_letter_password)
 {
     Password my_password;
-	int actual = my_password.count_leading_characters("Z");
-	ASSERT_EQ(1,actual);
+    int actual = my_password.count_leading_characters("Z");
+    ASSERT_EQ(1,actual);
 }
+
+TEST(PasswordTest, empty_letter_password)
+{
+    Password my_password;
+    int actual = my_password.count_leading_characters("");
+    ASSERT_EQ(0,actual);
+}
+
+TEST(PasswordTest, all_spaces_password)
+{
+    Password my_password;
+    int actual = my_password.count_leading_characters("   ");
+    ASSERT_EQ(3,actual);
+}
+
+TEST(PasswordTest, mixed_case_single_upper)
+{
+    Password my_password;
+    bool actual = my_password.has_mixed_case("Z");
+    ASSERT_EQ(false, actual);
+}
+
+TEST(PasswordTest, mixed_case_single_lower)
+{
+    Password my_password;
+    bool actual = my_password.has_mixed_case("z");
+    ASSERT_EQ(false, actual);
+}
+
+TEST(PasswordTest, mixed_case_2char_upper_first)
+{
+    Password my_password;
+    bool actual = my_password.has_mixed_case("Zz");
+    ASSERT_EQ(true, actual);
+}
+
+TEST(PasswordTest, mixed_case_2char_lower_first)
+{
+    Password my_password;
+    bool actual = my_password.has_mixed_case("zZ");
+    ASSERT_EQ(true, actual);
+}
+
+TEST(PasswordTest, mixed_case_empty_string)
+{
+    Password my_password;
+    bool actual = my_password.has_mixed_case("");
+    ASSERT_EQ(false, actual);
+}
+
+TEST(PasswordTest, mixed_case_3_spaces)
+{
+    Password my_password;
+    bool actual = my_password.has_mixed_case("  ");
+    ASSERT_EQ(false, actual);
+}
+
+
+/*
+#include <cstdio>
+#include <cstring>
+#include <cstdlib>
+#include <limits.h>
+TEST(PasswordTest, very_long_password)
+{
+    char *m = (char *)malloc(INT_MAX+2);
+    memset(m, 0, INT_MAX + 2);
+    memcpy(m, "a", INT_MAX);
+    Password my_password;
+    int actual = my_password.count_leading_characters(m);
+    ASSERT_EQ(INT_MAX, actual);
+}
+*/
