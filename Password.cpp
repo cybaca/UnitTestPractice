981045b8b2aacf5292b0a55ad6ac1b90522a6972
Cy <cbaca1@csuchico.edu>
2023-11-15 11:52:19 -0800

    Added set and authenticate functions, and test cases

--- Password.cpp
+++ Password.cpp
@@ -3,13 +3,67 @@
 
 using std::string;
 
-/*
-  The function receives a string counts how many times the same character 
-  occurs at the beginning of the string, before any other characters (or the
-  end of the string). The function is case-sensitive so 'Z' is different than
-  'z' and any ASCII characters are allowed.
-*/
-int Password::count_leading_characters(string phrase){
+/**
+ * constructor sets the default password to "ChicoCA-95929"
+ */
+Password::Password()
+{
+    set("ChicoCA-95929");
+}
+
+/**
+ * receives a password and sets the latest in pass_history to it if and
+ * only if it meets all criteria:
+ * 1. The password is at least 8 letters long
+ * 2. It has no more than 3 of the same leading characters
+ * 3. It has mixed case (at least one upper case and at least one lower
+ * case)
+ * 4. It was not a previous password in the history
+ */
+void
+Password::set(string s)
+{
+    if (s.length() < 8)
+        return;
+
+    if (count_leading_characters(s) > 3)
+        return;
+
+    if (!has_mixed_case(s))
+        return;
+
+    for (const auto &p : pass_history)
+        if (p.compare(s) == 0)
+            return;
+
+    pass_history.push_back(s);
+}
+
+/**
+ * receives a string and authenticates it against the latest password in
+ * the pass_history, returning true for an exact match or false when it does
+ * not match or if a password has not been set.
+ */
+bool
+Password::authenticate(string s)
+{
+    if (pass_history.empty())
+        return false;
+
+    return pass_history.back().compare(s) == 0;
+}
+
+
+
+/**
+ * The function receives a string counts how many times the same character 
+ * occurs at the beginning of the string, before any other characters (or the
+ * end of the string). The function is case-sensitive so 'Z' is different than
+ * 'z' and any ASCII characters are allowed.
+ */
+int
+Password::count_leading_characters(string phrase)
+{
     int repetition = 1;
     int index = 0;
 
@@ -20,11 +74,9 @@ int Password::count_leading_characters(string phrase){
     return repetition;
 }
 
-bool Password::has_mixed_case(string phrase)
+bool
+Password::has_mixed_case(string phrase)
 {
-    if (phrase.empty())
-        return false;
-
     bool has_lower = false;
     bool has_upper = false;
     for (int i = 0; i < phrase.length(); ++i) {
@@ -35,4 +87,18 @@ bool Password::has_mixed_case(string phrase)
     }
 
     return has_lower && has_upper;
+}
+
+bool
+Password::_has_mixed_case(string phrase)
+{
+    bool hasLower = false;
+    bool hasUpper = false;
+
+    for (char c: phrase) {
+        if (c >= 'a' && c <= 'z') hasLower = true;
+        if (c >= 'A' && c <= 'Z') hasUpper = true;
+    }
+
+    return hasLower && hasUpper;
 }
--- PasswordTest.cpp
+++ PasswordTest.cpp
@@ -19,6 +19,9 @@ TEST(PasswordTest, smoke_test)
     ASSERT_TRUE( 1 == 1 );
 }
 
+/*******************************************************************************
+ * LEADING
+ */
 TEST(PasswordTest, four_leading_char_password)
 {
     Password my_password;
@@ -33,27 +36,30 @@ TEST(PasswordTest, one_leading_char_password)
     ASSERT_EQ(1,actual);
 }
 
-TEST(PasswordTest, single_letter_password)
+TEST(PasswordTest, leading_single_letter_password)
 {
     Password my_password;
     int actual = my_password.count_leading_characters("Z");
     ASSERT_EQ(1,actual);
 }
 
-TEST(PasswordTest, empty_letter_password)
+TEST(PasswordTest, leading_empty_letter_password)
 {
     Password my_password;
     int actual = my_password.count_leading_characters("");
     ASSERT_EQ(0,actual);
 }
 
-TEST(PasswordTest, all_spaces_password)
+TEST(PasswordTest, leading_all_spaces_password)
 {
     Password my_password;
     int actual = my_password.count_leading_characters("   ");
     ASSERT_EQ(3,actual);
 }
 
+/*******************************************************************************
+ * MIXED
+ */
 TEST(PasswordTest, mixed_case_single_upper)
 {
     Password my_password;
@@ -82,6 +88,20 @@ TEST(PasswordTest, mixed_case_2char_lower_first)
     ASSERT_EQ(true, actual);
 }
 
+TEST(PasswordTest, mixed_case_upper_last)
+{
+    Password my_password;
+    bool actual = my_password.has_mixed_case("zzzzzzzzzzzzzzzzzzzZ");
+    ASSERT_EQ(true, actual);
+}
+
+TEST(PasswordTest, mixed_case_tilde)
+{
+    Password my_password;
+    bool actual = my_password.has_mixed_case("zzzzzzzzzzzzzzzzzzzZ~");
+    ASSERT_EQ(true, actual);
+}
+
 TEST(PasswordTest, mixed_case_empty_string)
 {
     Password my_password;
@@ -94,6 +114,156 @@ TEST(PasswordTest, mixed_case_3_spaces)
     Password my_password;
     bool actual = my_password.has_mixed_case("  ");
     ASSERT_EQ(false, actual);
+}
+
+TEST(PasswordTest, mixed_case_newline)
+{
+    Password my_password;
+    bool actual = my_password.has_mixed_case("\n");
+    ASSERT_EQ(false, actual);
+}
+
+TEST(PasswordTest, mixed_case_numbers)
+{
+    Password my_password;
+    bool actual = my_password.has_mixed_case("0x718abd");
+    ASSERT_EQ(false, actual);
+}
+
+
+TEST(PasswordTest, mixed_case_ul_numbers)
+{
+    Password my_password;
+    bool actual = my_password.has_mixed_case("helloWorld123");
+    ASSERT_EQ(true, actual);
+}
+
+TEST(PasswordTest, mixed_case_lower_numbers)
+{
+    Password my_password;
+    bool actual = my_password.has_mixed_case("123hello");
+    ASSERT_EQ(false, actual);
+}
+
+TEST(PasswordTest, mixed_case_special)
+{
+    Password my_password;
+    bool actual = my_password.has_mixed_case("Hello-World!!!");
+    ASSERT_EQ(true, actual);
+}
+
+TEST(PasswordTest, mixed_case_special_ws)
+{
+    Password my_password;
+    bool actual = my_password.has_mixed_case(" Hello-World!!! ");
+    ASSERT_EQ(true, actual);
+}
+
+TEST(PasswordTest, mixed_case_only_special)
+{
+    Password my_password;
+    bool actual = my_password.has_mixed_case("!@#$%^&*()");
+    ASSERT_EQ(false, actual);
+}
+
+TEST(PasswordTest, mixed_case_non_ascii)
+{
+    Password my_password;
+    bool actual = my_password.has_mixed_case("àáâäæãåāÇçÐĦĲ");
+    ASSERT_EQ(false, actual);
+}
+
+#include <climits>
+TEST(PasswordTest, mixed_case_long_string)
+{
+    // string a(INT_MAX, 'a');
+    string a(10000, 'a');
+    a += "Z";
+    Password my_password;
+    bool actual = my_password.has_mixed_case(a);
+    ASSERT_EQ(true, actual);
+}
+
+/*******************************************************************************
+ * AUTHENTICATE
+ */
+TEST(PasswordTest, authenticate_lt_eight)
+{
+    Password my_password;
+    my_password.set("95929");
+    bool actual = my_password.authenticate("95929");
+    ASSERT_EQ(false, actual);
+}
+
+TEST(PasswordTest, authenticate_leading_gt_three)
+{
+    Password my_password;
+    my_password.set("ggggg95928");
+    bool actual = my_password.authenticate("ggggg95929");
+    ASSERT_EQ(false, actual);
+}
+
+TEST(PasswordTest, authenticate_no_mixed)
+{
+    Password my_password;
+    my_password.set("abcdefghijk");
+    bool actual = my_password.authenticate("abcedfghijk");
+    ASSERT_EQ(false, actual);
+}
+
+TEST(PasswordTest, authenticate_default)
+{
+    Password my_password;
+    bool actual = my_password.authenticate("ChicoCA-95929");
+    ASSERT_EQ(true, actual);
+}
+
+TEST(PasswordTest, authenticate_not_default)
+{
+    Password my_password;
+    bool actual = my_password.authenticate("chicoCA-95929");
+    ASSERT_EQ(false, actual);
+}
+
+TEST(PasswordTest, authenticate_set_authenticate)
+{
+    Password my_password;
+    my_password.set("HelloWorld");
+    bool actual = my_password.authenticate("HelloWorld");
+    ASSERT_EQ(true, actual);
+}
+
+TEST(PasswordTest, authenticate_set_four_leading)
+{
+    Password my_password;
+    my_password.set("HHHHelloWorld");
+    bool actual = my_password.authenticate("ChicoCA-95929");
+    ASSERT_EQ(true, actual);
+}
+
+TEST(PasswordTest, authenticate_set_five_passwords)
+{
+    Password my_password;
+    my_password.set("HelloWorld");
+    my_password.set("HelloWorld1");
+    my_password.set("HelloWorld2");
+    my_password.set("HelloWorld3");
+    my_password.set("HelloWorld4");
+    bool actual = my_password.authenticate("HelloWorld4");
+    ASSERT_EQ(true, actual);
+}
+
+TEST(PasswordTest, authenticate_set_same_password)
+{
+    Password my_password;
+    my_password.set("HelloWorld");
+    my_password.set("HelloWorld1");
+    my_password.set("HelloWorld2");
+    my_password.set("HelloWorld3");
+    my_password.set("HelloWorld4");
+    my_password.set("HelloWorld1");
+    bool actual = my_password.authenticate("HelloWorld4");
+    ASSERT_EQ(true, actual);
 }
 
 
